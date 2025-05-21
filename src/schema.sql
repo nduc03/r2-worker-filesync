e80f4cb74053046ce2a3fd9452521c8c1610053e
Duc <48994241+nduc03@users.noreply.github.com>
2025-05-22 02:28:37 +0700

    Update schema.sql

--- src/schema.sql
+++ src/schema.sql
@@ -1,10 +1,6 @@
-DROP TABLE IF EXISTS file_metadata;
-CREATE TABLE file_metadata (
+CREATE TABLE IF NOT EXISTS file_metadata (
     id INTEGER PRIMARY KEY,
     object_name TEXT NOT NULL,
-    -- file_type TEXT NOT NULL,
     file_path TEXT NOT NULL,
-    -- size BIGINT NOT NULL,
     version INTEGER NOT NULL,
-    -- created_at TEXT
 );
